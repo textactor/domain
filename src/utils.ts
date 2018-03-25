b8543b69d2130f290cd87d1cee407c16fc08940d
Dumitru Cantea <canteadumitru@gmail.com>
2018-03-25 21:01:56 +0300

    seriesPromise & mapPromise

--- src/utils.ts
+++ src/utils.ts
@@ -9,6 +9,22 @@ export function uniq<T>(items: T[]) {
     return items.filter((value, index, self) => self.indexOf(value) === index);
 }
 
-export function eachSeries<T>(arr: any[], iteratorFn: (item: any) => Promise<T>) {
-    return arr.reduce((p, item) => p.then(() => iteratorFn(item)), Promise.resolve());
+export function seriesPromise<T, R>(arr: T[], iteratorFn: (item: T) => Promise<R>): Promise<R> {
+    return arr.reduce((p, item) => p.then(() => iteratorFn(item)), Promise.resolve(null));
+}
+
+export function mapPromise<T, R>(keys: T[], callback: (key: T) => Promise<R>):
+    Promise<Map<T, R>> {
+    const tasks = keys.map(key => callback(key).then(result => ({ key, result })));
+
+    return Promise.all(tasks)
+        .then(results => {
+            const response: Map<T, R> = new Map();
+
+            for (let item of results) {
+                response.set(item.key, item.result);
+            }
+
+            return response;
+        });
 }
