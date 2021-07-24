e81e0cf2c2e4fb8fbc0c9b7ecfddb3102f1ee38b
Dumitru C <canteadumitru@gmail.com>
2021-07-25 00:19:42 +0300

    v@0.3.0

--- src/entity-validator.ts
+++ src/entity-validator.ts
@@ -1,44 +1,46 @@
 import { BaseEntity } from "./entities";
 import { RepositoryUpdateData } from "./repository";
-import { validate as joiSchemaValidate, SchemaLike } from 'joi';
+import { AnySchema } from "joi";
 
 export interface EntityValidator<T extends BaseEntity> {
-    onCreate(data: T): T
-    onUpdate(data: RepositoryUpdateData<T>): RepositoryUpdateData<T>
+  onCreate(data: T): T;
+  onUpdate(data: RepositoryUpdateData<T>): RepositoryUpdateData<T>;
 }
 
 export interface EntityValidatorOptions {
-    createSchema: any
-    updateSchema: any
+  createSchema: any;
+  updateSchema: any;
 }
 
-export class JoiEntityValidator<T extends BaseEntity> implements EntityValidator<T> {
-    constructor(private options: EntityValidatorOptions) { }
+export class JoiEntityValidator<T extends BaseEntity>
+  implements EntityValidator<T>
+{
+  constructor(private options: EntityValidatorOptions) {}
 
-    onCreate(data: T) {
-        const result = validateSchema(this.options.createSchema, data);
-        if (result.error) {
-            throw result.error;
-        }
-        return result.value;
+  onCreate(data: T) {
+    const result = validateSchema(this.options.createSchema, data);
+    if (result.error) {
+      throw result.error;
     }
-    onUpdate(data: RepositoryUpdateData<T>) {
-        const result = validateSchema(this.options.updateSchema, data);
-        if (result.error) {
-            throw result.error;
-        }
-        return result.value;
+    return result.value;
+  }
+  onUpdate(data: RepositoryUpdateData<T>) {
+    const result = validateSchema(this.options.updateSchema, data);
+    if (result.error) {
+      throw result.error;
     }
+    return result.value;
+  }
 }
 
-function validateSchema<T>(schema: SchemaLike, data: T) {
-    return joiSchemaValidate<T>(data, schema, {
-        allowUnknown: false,
-        abortEarly: true,
-        convert: true,
-        noDefaults: false,
-        presence: 'optional',
-        stripUnknown: false,
-        skipFunctions: false,
-    });
+function validateSchema<T>(schema: AnySchema, data: T) {
+  return schema.validate(data, {
+    allowUnknown: false,
+    abortEarly: true,
+    convert: true,
+    noDefaults: false,
+    presence: "optional",
+    stripUnknown: false,
+    skipFunctions: false
+  });
 }
